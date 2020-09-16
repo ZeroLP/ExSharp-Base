6e281b53c9a8d041c3f9eec3807f80e90f920909
Zero <stubles227@gmail.com>
2020-09-16 17:45:36 +1000

    Updated to 10.19 and re-added LiveClientData API Usage

--- ExSharpBase/Game/OffsetManager.cs
+++ ExSharpBase/Game/OffsetManager.cs
@@ -8,14 +8,14 @@ namespace ExSharpBase.Game
 
         public class Instances
         {
-            //Version 10.15.330.344 [PUBLIC] // 8B 44 24 04 BA ? ? ? ? 2B D0
-            public static int LocalPlayer = BaseAddress + 0x34E1A34; // string xref blueHero -> Above "hero" subrtn
-            public static int Renderer = BaseAddress + 0x3508E90; // 8B 15 ? ? ? ? 83 EC 08 F3 // ["blurKernelSigma", +0x27F] // xref the string, move -0x27f there should be a dword.
-            public static int ViewMatrix = BaseAddress + 0x35061B0; // B9 ? ? ? ? E8 ? ? ? ? B9 ? ? ? ? E9 ? ? ? ? // First result: unk_0x...
+            //Version 10.19.335.4706 [PUBLIC] // 8B 44 24 04 BA ? ? ? ? 2B D0
+            public static int LocalPlayer = BaseAddress + 0x34E0280; // string xref blueHero -> Above "hero" subrtn
+            public static int Renderer = BaseAddress + 0x3507670; // 8B 15 ? ? ? ? 83 EC 08 F3 // ["blurKernelSigma", +0x27F] // xref the string, move -0x27f there should be a dword.
+            public static int ViewMatrix = BaseAddress + 0x3504990; // B9 ? ? ? ? E8 ? ? ? ? B9 ? ? ? ? E9 ? ? ? ? // First result: unk_0x...
 
             public static int SpellBook = 0x2708;
 
-            public static int UnderMouseObject = BaseAddress + 0x34D1A00; // 8B 0D ? ? ? ? 89 0D // Detected?
+            public static int UnderMouseObject = BaseAddress + 0x34D0254; // 8B 0D ? ? ? ? 89 0D
         }
 
         public class Object
