cd7e028d3beb8c809bf36fc5674254d2f30b9fc3
Zero <zerolp227@gmail.com>
2020-10-15 15:36:04 +1100

    Minor offset fix

--- ExSharpBase/Game/OffsetManager.cs
+++ ExSharpBase/Game/OffsetManager.cs
@@ -10,7 +10,7 @@ namespace ExSharpBase.Game
         {
             //Version 10.20.337.6669 [PUBLIC] // 8B 44 24 04 BA ? ? ? ? 2B D0
             public static int LocalPlayer = BaseAddress + 0x3504A14; // string xref blueHero -> Above "hero" subrtn
-            public static int Renderer = BaseAddress + 0x352CA10; // 8B 15 ? ? ? ? 83 EC 08 F3 // ["blurKernelSigma", +0x27F] // xref the string, move -0x27f there should be a dword.
+            public static int Renderer = BaseAddress + 0x3529CC8; // 8B 15 ? ? ? ? 83 EC 08 F3 // ["blurKernelSigma", +0x27F] // xref the string, move -0x27f there should be a dword.
             public static int ViewMatrix = BaseAddress + 0x3504480; // B9 ? ? ? ? E8 ? ? ? ? B9 ? ? ? ? E9 ? ? ? ? // First result: unk_0x...
 
             public static int SpellBook = 0x2720;
@@ -21,7 +21,7 @@ namespace ExSharpBase.Game
         public class Object
         {
             public static int ChampionName = 0x314C;
-            public static int Pos = 0x1D8;
+            public static int Pos = 0x220;
             public static int Name = 0x6C;
             public static int Visibility = 0x450;
             public static int AttackRange = 0x12D0;
