7fcbb62a1e189c6042f727f0d88f025fe54f4885
Zero <zerolp227@gmail.com>
2020-10-30 21:18:34 +1100

    Updated 10.22 Offsets

--- ExSharpBase/Game/OffsetManager.cs
+++ ExSharpBase/Game/OffsetManager.cs
@@ -9,13 +9,13 @@ namespace ExSharpBase.Game
         public class Instances
         {
             //Version 10.20.337.6669 [PUBLIC] // 8B 44 24 04 BA ? ? ? ? 2B D0
-            public static int LocalPlayer = BaseAddress + 0x3504A14; // string xref blueHero -> Above "hero" subrtn
-            public static int Renderer = BaseAddress + 0x3529CC8; // 8B 15 ? ? ? ? 83 EC 08 F3 // ["blurKernelSigma", +0x27F] // xref the string, move -0x27f there should be a dword.
-            public static int ViewMatrix = BaseAddress + 0x3504480; // B9 ? ? ? ? E8 ? ? ? ? B9 ? ? ? ? E9 ? ? ? ? // First result: unk_0x...
+            public static int LocalPlayer = BaseAddress + 0x3518034; // string xref blueHero -> Above "hero" subrtn
+            public static int Renderer = BaseAddress + 0x3540C74; // 8B 15 ? ? ? ? 83 EC 08 F3 // ["blurKernelSigma", +0x27F] // xref the string, move -0x27f there should be a dword.
+            public static int ViewMatrix = BaseAddress + 0x353DF38; // B9 ? ? ? ? E8 ? ? ? ? B9 ? ? ? ? E9 ? ? ? ? // First result: unk_0x...
 
             public static int SpellBook = 0x2720;
 
-            public static int UnderMouseObject = BaseAddress + 0x1C072C0; // 8B 0D ? ? ? ? 89 0D
+            public static int UnderMouseObject = BaseAddress + 0x1C1B0F0; // 8B 0D ? ? ? ? 89 0D
         }
 
         public class Object
